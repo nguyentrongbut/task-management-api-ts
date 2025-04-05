ddd204c327e33eb7f465c71efa1b56de52564160
nguyentrongbut <cloly031128@gmail.com>
2025-04-05 20:50:20 +0700

    feat: filter status

--- api/v1/controllers/task.controller.ts
+++ api/v1/controllers/task.controller.ts
@@ -2,9 +2,15 @@ import {Request, Response} from "express";
 import Task from "../models/tasks.model";
 
 export const index =  async (req: Request, res: Response) => {
-    const tasks = await Task.find({
-        deleted: false
-    });
+    const find = {
+        deleted: false,
+    }
+
+    if (req.query.status) {
+        find['status'] = req.query.status;
+    }
+
+    const tasks = await Task.find(find);
     res.json(tasks);
 }
 
