73ec3e8d6959aafe3dbfa398364fe7214d45cb31
nguyentrongbut <cloly031128@gmail.com>
2025-04-05 19:44:34 +0700

    feat: get tasks

--- index.ts
+++ index.ts
@@ -1,1 +1,23 @@
-console.log("OK")
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+import * as database from "./config/database";
+import Task from "./models/tasks.model";
+
+
+dotenv.config();
+
+database.connect();
+
+const app: Express = express();
+const port: number | string = process.env.PORT || 3000;
+
+app.get('/tasks', async (req: Request, res: Response) => {
+    const tasks = await Task.find({
+        deleted: false
+    });
+    res.json(tasks);
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+})
